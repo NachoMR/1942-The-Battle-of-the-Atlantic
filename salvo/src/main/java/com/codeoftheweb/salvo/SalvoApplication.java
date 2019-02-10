6ad64a509c041b1dcf5dc19857db115ac8675898
Nacho <igmarric@gmail.com>
2019-02-10 10:44:31 +0100

    Task2 8/8

--- salvo/src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ salvo/src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -26,8 +26,12 @@ public class SalvoApplication {
 			Player p4 = new Player("Tony", "Almeida", "t.almeida@ctu.gov", "PassTony");
 			Game g1 = new Game(LocalDateTime.now());
 			Game g2 = new Game(LocalDateTime.of(2019, Month.FEBRUARY, 04, 14, 30));
-			Game g3 = new Game(LocalDateTime.parse("2019-01-02T17:45:00"));
-			Game g = new Game(LocalDateTime.parse("2019-01-24T11:15:00"));
+			Game g3 = new Game(LocalDateTime.parse("2019-02-07T17:45:00"));
+			Game g4 = new Game(LocalDateTime.parse("2019-01-02T17:45:00"));
+			Game g5 = new Game(LocalDateTime.parse("2019-01-01T17:45:00"));
+			Game g6 = new Game(LocalDateTime.parse("2019-02-02T17:45:00"));
+			Game g7 = new Game(LocalDateTime.parse("2019-01-14T17:45:00"));
+			Game g8 = new Game(LocalDateTime.parse("2019-01-30T17:45:00"));
 
 			playerRepository.save(p1);
 			playerRepository.save(p2);
@@ -36,16 +40,26 @@ public class SalvoApplication {
 			gameRepository.save(g1);
 			gameRepository.save(g2);
 			gameRepository.save(g3);
+			gameRepository.save(g4);
+			gameRepository.save(g5);
+			gameRepository.save(g6);
+			gameRepository.save(g7);
+			gameRepository.save(g8);
 			gamePlayerRepository.save(new GamePlayer(g1, p1));
 			gamePlayerRepository.save(new GamePlayer(g1, p2));
-			gamePlayerRepository.save(new GamePlayer(g2, p3));
-			gamePlayerRepository.save(new GamePlayer(g2, p4));
-			gamePlayerRepository.save(new GamePlayer(g2, p4));
+			gamePlayerRepository.save(new GamePlayer(g2, p1));
+			gamePlayerRepository.save(new GamePlayer(g2, p2));
+			gamePlayerRepository.save(new GamePlayer(g3, p2));
 			gamePlayerRepository.save(new GamePlayer(g3, p4));
-			gamePlayerRepository.save(new GamePlayer(g3, p4));
-
+			gamePlayerRepository.save(new GamePlayer(g4, p2));
+			gamePlayerRepository.save(new GamePlayer(g4, p1));
+			gamePlayerRepository.save(new GamePlayer(g5, p4));
+			gamePlayerRepository.save(new GamePlayer(g5, p1));
+			gamePlayerRepository.save(new GamePlayer(g6, p3));
+			gamePlayerRepository.save(new GamePlayer(g7, p4));
+			gamePlayerRepository.save(new GamePlayer(g8, p3));
+			gamePlayerRepository.save(new GamePlayer(g8, p4));
 		};
 	}
 
 }
-
---
+++ salvo/src/main/resources/static/web/games.js
@@ -0,0 +1,38 @@
+//================ VUE VAR DECLARATION ===================
+
+var myVue = new Vue({
+	el: "#app",
+	data: {
+		games: [],
+		gameplayers: []
+	},
+	methods: {
+		//		getGames: 
+	},
+	created: function () {
+		fetch('http://localhost:8080/api/games', {
+				method: 'GET',
+			})
+			.then(function (response) {
+				return response.json();
+				//console.log("First .then() works!");
+			})
+			.then(function (games) {
+				console.log(JSON.stringify(games));
+				//console.log(games);
+				console.log("Second .then() works!");
+				myVue.games = games;
+				myVue.gameplayers = games.gameplayers;
+			}).catch(function (error) {
+				alert(error);
+				//console.log("Error during fetch" + error.message)
+			});
+	},
+	computed: {}
+});
+
+
+//myVue.getGames();
+
+
+// ====================== FUNCTIONS ======================
